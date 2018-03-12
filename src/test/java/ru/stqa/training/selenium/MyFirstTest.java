291d7d28d90dc2e21805e71514835559bf04b2e6
virtus80 <virtus80@mail.ru>
2018-03-12 15:59:33 +0200

    Task1
    - removed extra code

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -26,7 +24,6 @@ public class MyFirstTest {
     @Test
     public void myFirstTest() {
         driver.navigate().to("http://www.google.com");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.name("q")).sendKeys("webdriver\n");
 //        driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
