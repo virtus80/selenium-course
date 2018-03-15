3b2f442a3bf74925c72bedc640403d97f7f8efb1
virtus80 <virtus80@mail.ru>
2018-03-15 15:00:43 +0200

    Added launching Firefox Nightly
    - Added launching Firefox Nightly

--- src/test/java/ru/stqa/training/selenium/litecart/LoginTest.java
+++ src/test/java/ru/stqa/training/selenium/litecart/LoginTest.java
@@ -4,9 +4,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 public class LoginTest {
 
@@ -15,7 +26,29 @@ public class LoginTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+//        driver = new InternetExplorerDriver();
+//        FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+//        driver = new FirefoxDriver(options);
+
+// working code
+        FirefoxOptions options = new FirefoxOptions();
+        FirefoxBinary bin = new FirefoxBinary(new File("c:\\Program Files\\Firefox Nightly\\firefox.exe"));
+        options.setBinary(bin);
+        driver = new FirefoxDriver(options);
+
+//      DesiredCapabilities caps = new DesiredCapabilities();
+//      caps.setCapability(FirefoxDriver.MARIONETTE, true);
+//      driver = new FirefoxDriver(caps);
+
+//        ChromeOptions options = new ChromeOptions();
+//        options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+//        options.addArguments("start-maximized");
+//        driver = new ChromeDriver(options);
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+//        driver = new InternetExplorerDriver(caps);
+//        System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver, 10);
     }
 
