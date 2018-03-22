07dfd4fdd2e97d1b9c9477a3eb039482eb2ecef1
virtus80 <virtus80@mail.ru>
2018-03-22 15:42:12 +0200

    Hometask10
    - added comparing font size and color on product and main pages

--- src/test/java/ru/stqa/training/selenium/litecart/ShopMainPageTest.java
+++ src/test/java/ru/stqa/training/selenium/litecart/ShopMainPageTest.java
@@ -3,17 +3,21 @@ package ru.stqa.training.selenium.litecart;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
+
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
-public class ShopMainPageTest extends TestBase{
+public class ShopMainPageTest extends TestBase {
 
     @Test
     public void testStickerOnImage() {
         driver.get("http://localhost/litecart/en/");
         List<WebElement> images = driver.findElements(By.cssSelector("li.product"));
-        for(WebElement image : images) {
+        for (WebElement image : images) {
             List<WebElement> stickers = image.findElements(By.cssSelector("div.sticker"));
             assertEquals(stickers.size(), 1);
         }
@@ -22,23 +26,55 @@ public class ShopMainPageTest extends TestBase{
     @Test
     public void testCompareProductPage() {
         driver.get("http://localhost/litecart/en/");
-        WebElement productOnManePage = driver.findElement(By.cssSelector("#box-campaigns li.product"));
-        String productNameOnManePage = getProductAttribute(productOnManePage, "name");
-        String regularPriceOnManePage = getProductAttribute(productOnManePage, "regular-price");
-        String campaignPriceOnManePage = getProductAttribute(productOnManePage, "campaign-price");
-        productOnManePage.click();
+        WebElement productOnMainPage = driver.findElement(By.cssSelector("#box-campaigns li.product"));
+        WebElement productNameOnMainPage = getProductAttribute(productOnMainPage, "name");
+        WebElement regularPriceOnMainPage = getProductAttribute(productOnMainPage, "regular-price");
+        WebElement campaignPriceOnMainPage = getProductAttribute(productOnMainPage, "campaign-price");
+        String textProductNameOnMainPage = productNameOnMainPage.getText();
+        String textRegularPriceOnMainPage = regularPriceOnMainPage.getText();
+        String textCampaignPriceOnMainPage = campaignPriceOnMainPage.getText();
+        //compare font style on main page
+        assertEquals("s", regularPriceOnMainPage.getAttribute("localName"));
+        assertEquals("strong", campaignPriceOnMainPage.getAttribute("localName"));
+        //compare font color on main page
+        String[] colorRegularPriceOnMainPage = getElementColorRgbArray(regularPriceOnMainPage);
+        String[] colorCampaignPriceOnMainPage = getElementColorRgbArray(campaignPriceOnMainPage);
+        assertEquals(colorRegularPriceOnMainPage[0], colorRegularPriceOnMainPage[1], colorRegularPriceOnMainPage[2]);
+        assertEquals(colorCampaignPriceOnMainPage[1], colorCampaignPriceOnMainPage[2]);
+        //compare font size on main page
+        assertTrue(getFontSize(campaignPriceOnMainPage) > getFontSize(regularPriceOnMainPage));
+
+        productOnMainPage.click();
         WebElement productOnProductPage = driver.findElement(By.cssSelector("#box-product"));
-        String productNameOnProductPage = getProductAttribute(productOnProductPage, "title");
-        String regularPriceOnProductPage = getProductAttribute(productOnProductPage, "regular-price");
-        String campaignPriceOnProductPage = getProductAttribute(productOnProductPage, "campaign-price");
-        assertEquals(productNameOnManePage, productNameOnProductPage);
-        assertEquals(regularPriceOnManePage, regularPriceOnProductPage);
-        assertEquals(campaignPriceOnManePage, campaignPriceOnProductPage);
+        WebElement productNameOnProductPage = getProductAttribute(productOnProductPage, "title");
+        WebElement regularPriceOnProductPage = getProductAttribute(productOnProductPage, "regular-price");
+        WebElement campaignPriceOnProductPage = getProductAttribute(productOnProductPage, "campaign-price");
+        //compare text on main and product pages
+        assertEquals(textProductNameOnMainPage, productNameOnProductPage.getText());
+        assertEquals(textRegularPriceOnMainPage, regularPriceOnProductPage.getText());
+        assertEquals(textCampaignPriceOnMainPage, campaignPriceOnProductPage.getText());
+        //compare font style on product page
+        assertEquals("s", regularPriceOnProductPage.getAttribute("localName"));
+        assertEquals("strong", campaignPriceOnProductPage.getAttribute("localName"));
+        //compare font color on product page
+        String[] colorRegularPriceOnProductPage = getElementColorRgbArray(regularPriceOnProductPage);
+        String[] colorCampaignPriceOnProductPage = getElementColorRgbArray(campaignPriceOnProductPage);
+        assertEquals(colorRegularPriceOnProductPage[0], colorRegularPriceOnProductPage[1], colorRegularPriceOnProductPage[2]);
+        assertEquals(colorCampaignPriceOnProductPage[1], colorCampaignPriceOnProductPage[2]);
+        //compare font size on product page
+        assertTrue(getFontSize(campaignPriceOnProductPage) > getFontSize(regularPriceOnProductPage));
     }
 
-    private String getProductAttribute(WebElement element, String className) {
-        return element.findElement(By.className(className)).getText();
+    private WebElement getProductAttribute(WebElement element, String className) {
+        return element.findElement(By.className(className));
     }
 
+    private String[] getElementColorRgbArray(WebElement element) {
+        String color = Color.fromString(element.getCssValue("color")).asRgb();
+        return color.replaceAll("[rgb()]", "").split(", ");
+    }
 
+    private double getFontSize(WebElement element) {
+        return Double.parseDouble(element.getCssValue("font-size").replace("px", ""));
+    }
 }
