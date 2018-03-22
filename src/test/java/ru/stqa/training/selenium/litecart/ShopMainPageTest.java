c0a39949970133cff17f686bf54c4e7003553067
virtus80 <virtus80@mail.ru>
2018-03-22 10:34:46 +0200

    Hometask 10
    -added test comparing product's attributes on main and product pages

--- src/test/java/ru/stqa/training/selenium/litecart/ShopMainPageTest.java
+++ src/test/java/ru/stqa/training/selenium/litecart/ShopMainPageTest.java
@@ -18,4 +18,27 @@ public class ShopMainPageTest extends TestBase{
             assertEquals(stickers.size(), 1);
         }
     }
+
+    @Test
+    public void testCompareProductPage() {
+        driver.get("http://localhost/litecart/en/");
+        WebElement productOnManePage = driver.findElement(By.cssSelector("#box-campaigns li.product"));
+        String productNameOnManePage = getProductAttribute(productOnManePage, "name");
+        String regularPriceOnManePage = getProductAttribute(productOnManePage, "regular-price");
+        String campaignPriceOnManePage = getProductAttribute(productOnManePage, "campaign-price");
+        productOnManePage.click();
+        WebElement productOnProductPage = driver.findElement(By.cssSelector("#box-product"));
+        String productNameOnProductPage = getProductAttribute(productOnProductPage, "title");
+        String regularPriceOnProductPage = getProductAttribute(productOnProductPage, "regular-price");
+        String campaignPriceOnProductPage = getProductAttribute(productOnProductPage, "campaign-price");
+        assertEquals(productNameOnManePage, productNameOnProductPage);
+        assertEquals(regularPriceOnManePage, regularPriceOnProductPage);
+        assertEquals(campaignPriceOnManePage, campaignPriceOnProductPage);
+    }
+
+    private String getProductAttribute(WebElement element, String className) {
+        return element.findElement(By.className(className)).getText();
+    }
+
+
 }
